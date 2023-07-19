6fb1321c533a3bf4782ff2671b1200ee3630eca1
Akmere Almeida <akmerealmeida@pop-os.localdomain>
2023-07-19 19:45:19 -0300

    update professional experience and education

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -3,11 +3,11 @@ module.exports = {
   title: 'Mobile Android Developer',
   facts: {
     'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'LinkedIn': `<a href="https://www.linkedin.com/in/akmere-almeida"><i class="fa-brands fa-linkedin fact-icon"></i>Akme Re</a>`,
+    'LinkedIn': `<a href="https://www.linkedin.com/in/akmere-almeida"><i class="fa-brands fa-linkedin fact-icon"></i>Akme Re Almeida</a>`,
     'Email': '<a href="mailto:akmerejf@gmail.com"><i class="fa fa-envelope fact-icon"></i>akmerejf@gmail.com</a>'
   },
   skills: [
-    ['Comedy', 100],
+    ['Android ', 100],
     ['Love', 100],
     ['Theater', 100],
     ['Tragedy', 100],
@@ -22,27 +22,43 @@ module.exports = {
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
+      title: 'Software Consultant/Specialist (Mobile) - Globant',
+      period: 'Sept 2022 - currently working',
+      skills: ['android', 'flutter', 'mobile', 'consultancy'],
       contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
+As an Android Developer at a large Media client, successfully developed and maintained a top-rated media application for both mobile and TV devices. Focused on creating scalable, testable, and high-quality code, implemented a robust architecture using the latest Android SDK and best practices. Established a comprehensive unit and integration testing framework, enhancing code quality and reducing production defects. Collaborated with cross-functional teams to prioritize and implement new features, resulting in increased user engagement. Optimized app performance, significantly improving load times and reducing crashes. Integrated CI/CD pipelines to streamline the development process. Improved experience in Flutter SDK by implementing a fresh mobile application which consists of managing a websocket connection to let the user join in a Scrum Poker session previously created by a web client. Developing this application, went through all steps of development, from planning features, helping achieve better UI/UX, creating CI/CD pipelines, code testing and implementation to delivering the application to the final users.      
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
+      title: 'Software Consultant/Specialist (Android) - ThoughtWorks',
+      period: 'December 2020 to Sept 2022',
+      skills: ['android', 'jetpack compose', 'consultancy'],
       contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+Worked as a Senior Software Consultant for the majority of projects. Delivering high performance, scalable and well tested Android applications, as well as solving architectural bottlenecks with state of the art solutions in the mobile ecosystem. Worked side by side with the UI/UX team, Backend team, and PMs in order to deliver the best possible solution to our clients. Developed native Android and multiplatform SDKs as well as integrated them in the end user Android applications. Integrated both Rest and GraphQL APIs for three different kinds of Android projects, using third party and inhouse http client solutions.
 `
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: 'Software Engineer/Specialist (Android) - Grupo Zap',
+      period: 'Feb 2020 to Dec 2020',
+      skills: ['android', 'kotlin multiplatform', 'flutter', 'real estate'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+Joined Grupo Zap to integrate the team that would take care of three mobile applications in two platforms, which are marketplaces to showcase listings of properties. They developed a multiplatform SDK built on top of Kotlin Native to handle metrics between native applications (Android and iOS) and all kinds of Analytics providers, like Firebase, Google Analytics, Facebook Analytics and so on. After that, Akme Re became part of a project to start using BDD on mobile applications, creating a multiplatform project using Appium + Cucumber and integrating it on mobile applications CI/CD flow. After that, they went back to native Android development / improvement in order to create Android feature modules with modern Android development and Flutter Platform Channels adding these modules/features into existing native applications, allowing for seamless cross-platform functionality.
+`
+    },
+    {
+      title: 'Software Engineer/Specialist (Android) - PayGo',
+      period: 'Feb 2019 to Feb 2020',
+      skills: ['android', 'payment methods'],
+      contents: `
+Developed from scratch a Payment application which communicates with PinPad devices through Bluetooth, using the already developed SDKs. Started on native Android SDKs development / improvement when the project grew and more developers were hired. Built CI/CD pipelines and versioning of SDKs to achieve high scale management.
+`
+    },
+    {
+      title: 'Software Engineer/Specialist (Android) - Vibe Desenvolvimento',
+      period: 'Jul 2018 to Jan 2019',
+      skills: ['android', 'banking'],
+      contents: `
+Developed and improved features for a Banking Android application. Being the only Android developer in the team, Akme Re had autonomy to study and implement new technologies as well as participate in the decision about new features.
 `
     }
   ],
